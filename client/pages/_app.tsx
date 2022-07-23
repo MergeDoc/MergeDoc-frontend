8fe4d9a266fe281a8321f9afd3665200d0c71d48
doheez <doheedev@gmail.com>
2022-07-23 23:21:16 +0900

    feat: Create theme palette for app

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {ThemeProvider} from "@mui/material";
+import theme from "../styles/theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
 }
 
 export default MyApp
